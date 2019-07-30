6ee805c6fdf1640877b0b178c84a42f21e821e77
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-07-30 11:06:47 -0600

    atomic_int increment on handlers

--- evt.cpp
+++ evt.cpp
@@ -6,29 +6,23 @@
 std::default_random_engine gen;
 std::uniform_int_distribution<int> distribution(0,2);
 
-std::atomic_flag g_mtx = ATOMIC_FLAG_INIT;
-int counter = 0;
+//std::atomic_flag g_mtx = ATOMIC_FLAG_INIT;
+std::atomic_int counter = 0;
 const int numIters = 10000;
 void add(int i, int j) 
 { 
     for(auto k = 0; k < numIters; ++k) i+j;
-    while(g_mtx.test_and_set(std::memory_order_acquire));
     ++counter;
-    g_mtx.clear(std::memory_order_release);
 }
 void sub(int i, int j)
 {
     for(auto k = 0; k < numIters; ++k) i-j;
-    while(g_mtx.test_and_set(std::memory_order_acquire));
     ++counter;
-    g_mtx.clear(std::memory_order_release);
 }
 void mult(int i, int j)
 {
     for(auto k = 0; k < numIters; ++k) i*j;
-    while(g_mtx.test_and_set(std::memory_order_acquire));
     ++counter;
-    g_mtx.clear(std::memory_order_release);
 }
 
 using func_type = void(int, int);
@@ -41,21 +35,13 @@ int main()
         a.add_handler(0, add);
         a.add_handler(1, sub);
         a.add_handler(2, mult);
-        for(auto i = 0; i < 10000000; ++i)
+        for(auto i = 0; i < 1000000; ++i)
         {
             auto r = distribution(gen);
             a.post(evt::EventSystem<func_type>::Event(r, std::make_tuple(i-r,i)));
         }
         std::cout << "finished generating events\n";
-/*        a.wait();
-        std::cout << "waiting for input\n";
-        int j;
-        std::cin >> j;
-        for(auto i = 0; i < 10000000; ++i)
-        {
-            auto r = distribution(gen);
-            a.post(evt::EventSystem<func_type>::Event(r, std::make_tuple(i-r,i)));
-        }*/
+        std::cout << "waiting for events to be handled\n";
     }
     auto end = steady_clock::now();
     auto duration = end - start;
