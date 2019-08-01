22ced37ec780b754576db3b59ad7fe6774a312d6
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-08-01 15:22:45 -0600

    include bind port

--- testclient.py
+++ testclient.py
@@ -4,6 +4,7 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--addr', type=str)
+parser.add_argument('--bport',type=int)
 parser.add_argument('--port', type=int)
 parser.add_argument("--msg", type=str)
 parser.add_argument('-m', '--machine', type=str, default='machine1')
@@ -12,6 +13,7 @@ args = parser.parse_args()
 i = 0
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(('',args.bport))
     while True:
         msg = '{}: {}'.format(args.machine, args.msg)
         bin_data = (msg+' ').encode('utf-8') + i.to_bytes(4, byteorder='little')
