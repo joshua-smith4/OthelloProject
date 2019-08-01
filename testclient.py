7ded643061d634a6d93737e349d9602c908ecfe5
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-08-01 15:11:27 -0600

    dynamic address in testclient

--- testclient.py
+++ testclient.py
@@ -3,6 +3,7 @@ import argparse
 import time
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--addr', type=str)
 parser.add_argument('--port', type=int)
 parser.add_argument("--msg", type=str)
 parser.add_argument('-m', '--machine', type=str, default='machine1')
@@ -14,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     while True:
         msg = '{}: {}'.format(args.machine, args.msg)
         bin_data = (msg+' ').encode('utf-8') + i.to_bytes(4, byteorder='little')
-        s.sendto(bin_data, ('127.0.0.1', args.port))
+        s.sendto(bin_data, (args.addr, args.port))
         data = s.recvfrom(1024)
         print(data)
         i+=1
